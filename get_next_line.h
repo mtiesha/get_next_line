44372861fcb8e7a5aa959ea45810fff805cce19c
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-12 07:01:56 +0300

    create gnl

---
+++ get_next_line.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtiesha <mtiesha@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/12 06:57:20 by mtiesha           #+#    #+#             */
+/*   Updated: 2021/11/12 06:57:20 by mtiesha          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+#endif
