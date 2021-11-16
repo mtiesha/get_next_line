05245ead256c57cde4aff3ce46322568ed7707b5
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-16 21:03:30 +0300

    first try

--- get_next_line.c
+++ get_next_line.c
@@ -1,7 +1,7 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libft.h                                            :+:      :+:    :+:   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mtiesha <mtiesha@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
@@ -12,8 +12,19 @@
 
 #include "get_next_line.h"
 
-char	*get_next_line(int fd)
+void	*ft_call_struct(char *buff, int fd, struct_t fd_stack)
 {
+	while (fd != 
+}
+
+char	*ft_get_next_line(int fd)
+{
+	char		*heap;
+	static struct_t	fd_stack;
+
+	heap = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
+	if (fd >= 65536 || fd < 0 || !heap)
+		return (NULL);
+	ft_call_struct(heap, fd, fd_stack);
 	
-	return (NULL);	
 }
