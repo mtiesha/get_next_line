4fa9167fdc962e5335624cb111280d8990dd45a0
mtiesha <86960854+mtiesha@users.noreply.github.com>
2021-11-21 20:27:00 +0300

    return to lists

--- get_next_line.c
+++ get_next_line.c
@@ -12,27 +12,68 @@
 
 #include "get_next_line.h"
 
-char	*ft_reader(const int fd, char **fd_stack)
+static void	*ft_content_maker(char *buff)
 {
-	char	*heap;
+	while (
+}
 
-	heap = (char *)malloc(BUFF_SIZE * sizeof(char));
-	if (!heap || (read(fd, heap, BUFF_SIZE) < 0))
-		return (NULL);
-	heap[BUFF_SIZE] = 0;
-	if (fd_stack[fd][0] != NULL)
+static t_list	*ft_lstnew(int id, char *content, int flag)
+{
+	t_list	*p;
+
+	if (!flag)
 	{
-		ft_strjoin(fd_stack[fd], heap);
+		p = (t_list *)malloc(1 * sizeof(t_list));
+		if (p != NULL && ft_content_maker(content))
+		{
+			ft_content_maker(
+			//if (!content)
+			//	p -> content = NULL;
+			//else
+			//	p -> content = content;
+			//p -> next = NULL;
+			return (p);
+		}
 	}
+	
+	return (NULL);
+}
+
+static void	*ft_call_struct(char *buff, int fd, t_list *s_stack)
+{
+	t_list	*s_str;
+	t_list	*s_fd_stack;
+
+	s_fd_stack = s_stack;
+	while (fd != s_fd_stack -> id || s_fd_stack -> next != NULL)
+		s_fd_stack = s_fd_stack -> next;
+	if (fd != s_fd_stack -> id)
+	{
+		if (read(fd, &buff, BUFF_SIZE))
+		{
+			s_str = ft_lstnew(fd, buff, 1);
+			if (s_str != NULL)
+				s_fd_stack -> next = s_str;
+		}
+	}
+	else if (s_fd_stack -> next == NULL)
+	{
+		read(fd, &buff, BUFF_SIZE);
+		s_str = ft_lstnew(fd, buff, 0);
+	}
+	return (NULL);
 }
 
 char	*ft_get_next_line(int fd)
 {
-	char		*ret;
-	static char	**fd_stack[MAX_FD];
+	char			*heap;
+	static struct_t	*s_fd;
 
-	if (fd > MAX_FD || fd < 0 || fd == 1)
+	heap = (char *)malloc((BUFF_SIZE + 1) * sizeof(char));
+	if (fd > OPEN_MAX || fd < 0 || !heap)// add limits.h in header
 		return (NULL);
-	fd_stack[MAX_FD][0] = '0';
-	return (ft_reader(fd, fd_stack));
+	heap[BUFF_SIZE] = 0;
+	if (!ft_call_struct(heap, fd, s_fd)
+		return (NULL);
+	return (heap);
 }
